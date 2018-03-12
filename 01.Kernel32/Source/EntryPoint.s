673672350451828d975cb00835677b1718ed85cd
JLGGG <blockboy99@nate.com>
2018-03-12 17:32:21 +0900

    adding IA-32e mode

--- 01.Kernel32/Source/EntryPoint.s
+++ 01.Kernel32/Source/EntryPoint.s
@@ -34,11 +34,11 @@ START:
 	mov eax, 0x4000003B
 	mov cr0, eax
 
-	jmp dword 0x08: (PROTECTEDMODE - $$ + 0x10000)
+	jmp dword 0x18: (PROTECTEDMODE - $$ + 0x10000)
 
 [BITS 32]
 PROTECTEDMODE:
-	mov ax, 0x10
+	mov ax, 0x20
 	mov ds, ax
 	mov es, ax
 	mov fs, ax
@@ -54,7 +54,7 @@ PROTECTEDMODE:
 	call PRINTMESSAGE
 	add esp, 12
 
-	jmp dword 0x08: 0x10200 ; move c kernel code
+	jmp dword 0x18: 0x10200 ; move c kernel code
 
 ; function code area
 PRINTMESSAGE:
@@ -118,6 +118,22 @@ GDT:
 		db 0x00
 		db 0x00
 		db 0x00
+		db 0x00
+
+	IA_32eCODEDESCRIPTOR:
+		dw 0xFFFF
+		dw 0x0000
+		db 0x00
+		db 0x9A
+		db 0xAF
+		db 0x00
+
+	IA_32eDATADESCRIPTOR:
+		dw 0xFFFF
+		dw 0x0000
+		db 0x00
+		db 0x92
+		db 0xAF
 		db 0x00
 
 	CODEDESCRIPTOR:
--- 01.Kernel32/Source/Main.c
+++ 01.Kernel32/Source/Main.c
@@ -1,5 +1,6 @@
 #include "Types.h"
 #include "Page.h"
+#include "ModeSwitch.h"
 
 void kPrintString(int iX, int iY, const char* pcString);
 BOOL kInitializeKernel64Area(void);
@@ -8,6 +9,8 @@ BOOL kIsMemoryEnough(void);
 void Main(void)
 {
 	DWORD i;
+	DWORD dwEAX, dwEBX, dwECX, dwEDX;
+	char vcVendorString[13]={0,};
 
 	kPrintString(0,3, "C Language Kernel Start....................[Pass]");
 
@@ -36,6 +39,28 @@ void Main(void)
 	kInitializePageTables();
 	kPrintString(44,6,"Pass");
 
+	kReadCPUID(0x00, &dwEAX, &dwEBX, &dwECX, &dwEDX);
+	*((DWORD*)vcVendorString) = dwEBX;
+	*((DWORD*)vcVendorString + 1) = dwEDX;
+	*((DWORD*)vcVendorString + 2) = dwECX;
+	kPrintString(0,7, "Processor Vendor String....................[            ]");
+	kPrintString(44,7, vcVendorString);
+
+	kReadCPUID(0x80000001, &dwEAX, &dwEBX, &dwECX, &dwEDX);
+	kPrintString(0,8, "64bit Mode Support Check...................[    ]");
+	if(dwEDX & (1<<29))
+	{
+		kPrintString(44,8,"Pass");
+	}
+	else
+	{
+		kPrintString(44,8, "Fail");
+		kPrintString(0,9, "This processor does not support 64bit mode~!!");
+		while(1);
+	}
+
+	kPrintString(0,9, "Switch To IA-32e Mode");
+	//kSwitchAndExecute64bitKernel();
 	while(1);
 }
 
