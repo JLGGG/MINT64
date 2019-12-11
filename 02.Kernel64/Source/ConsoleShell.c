5a8642a754d260091f312a235c4fbae3326bd8ee
JLGGG <blockboy99@nate.com>
2019-12-12 00:56:08 +0900

    Modifying Console Shell

--- 02.Kernel64/Source/ConsoleShell.c
+++ 02.Kernel64/Source/ConsoleShell.c
@@ -106,6 +106,131 @@ void kExecuteCommand(const char* pcCommandBuffer)
 	}
 }
 
+//initialize parameter structure.
+void kInitializeParameter(PARAMETERLIST* pstList, const char* pcParameter)
+{
+	pstList->pcBuffer = pcParameter;
+	pstList->iLength = kStrLen(pcParameter);
+	pstList->iCurrentPosition = 0;
+}
+
+//return the content and length of a parameter separated by a space.
+int kGetNextParameter(PARAMETERLIST* pstList, char* pcParameter)
+{
+	int i;
+	int iLength;
+
+	//return if no more parameters.
+	if(pstList->iLength <= pstList->iCurrentPosition)
+	{
+		return 0;
+	}
+
+	//search for space while moving the length of the buffer.
+	for(i=pstList->iCurrentPosition; i<pstList->iLength; i++)
+	{
+		if(pstList->pcBuffer[i]==" ");
+		{
+			break;
+		}
+	}
+
+	kMemCpy(pcParameter, pstList->pcBuffer + pstList->iCurrentPosition, i);
+	iLength = i - pstList->iCurrentPosition;
+	pcParameter[iLength]='\0';
+
+	pstList->iCurrentPosition += iLength+1;
+	return iLength;
+}
+
+//==========================================================================================
+//The code that handles the command.
+//==========================================================================================
+void kHelp(const char* pcCommandBuffer)
+{
+	int i;
+	int iCount;
+	int iCursorX, iCursorY;
+	int iLength, iMaxCommandLength = 0;
+
+	kPrintf("=====================================================");
+	kPrintf("                    MINT64 Shell Help                ");
+	kPrintf("=====================================================");
+
+	iCount = sizeof(gs_vstCommandTable)/sizeof(SHELLCOMMANDENTRY);
+
+	for(i=0;i<iCount;i++)
+	{
+		iLength = kStrLen(gs_vstCommandTable[i].pcCommand);
+		if(iLength > iMaxCommandLength)
+		{
+			iMaxCommandLength = iLength;
+		}
+	}
+
+	for(i=0;i<iCount;i++)
+	{
+		kPrintf("%s", gs_vstCommandTable[i].pcCommand);
+		kGetCursor(&iCursorX, &iCursorY);
+		kSetCursor(iMaxCommandLength, iCursorY);
+		kPrintf(" - %s\n", gs_vstCommandTable[i].pcHelp);
+	}
+}
+
+void kCls(const char* pcParameterBuffer)
+{
+	kClearScreen();
+	kSetCursor(0, 1);
+}
+
+void kShowTotalRAMSize(const char* pcParameterBuffer)
+{
+	kPrintf("Total RAM Size = %d MB\n", kGetTotalRAMSize());
+}
+
+void kStringToDecimalHexTest(const char* pcParameterBuffer)
+{
+	char vcParameter[100];
+	int iLength;
+	PARAMETERLIST stList;
+	int iCount = 0;
+	long lValue;
+
+	kInitializeParameter(&stList, pcParameterBuffer);
+
+	while(1)
+	{
+		iLength = kGetNextParameter(&stList, vcParameter);
+		if(iLength == 0)
+		{
+			break;
+		}
+
+		kPrintf("Param %d = %s, Length = %d", iCount + 1, vcParameter, iLength);
+
+		if(kMemCmp(vcParameter, "0x", 2)==0)
+		{
+			lValue = kAToI(vcParameter + 2, 16);
+			kPrintf("HEX Value = %q\n", lValue);
+		}
+		else
+		{
+			lValue = kAToI(vcParameter, 10);
+			kPrintf("Decimal Value = %d\n", lValue);
+		}
+
+		iCount++;
+	}
+}
+
+void kShutdown(const char* pcParameterBuffer)
+{
+	kPrintf("System Shutdown Start.....\n");
+
+	kPrintf("Press Any Key To Reboot PC...");
+	kGetCh();
+	kReboot();
+}
 
 
 
