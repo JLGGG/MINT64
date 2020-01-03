73c1c2769edf990c3a2e15546ef0215e07094e9f
JLGGG <blockboy99@nate.com>
2020-01-03 19:06:27 +0900

    Adding Utility Function.

--- 02.Kernel64/Source/Utility.c
+++ 02.Kernel64/Source/Utility.c
@@ -105,4 +105,182 @@ void kCheckTotalRAMSize(void)
 	gs_qwTotalRAMMBSize = (QWORD)pdwCurrentAddress / 0x100000;
 }
 
+//Returns the amount of RAM
+QWORD kGetTotalRAMSize(void)
+{
+	return gs_qwTotalRAMMBSize;
+}
+
+//Internal implementation of atoi()
+long kAToI(const char* pcBuffer, int iRadix)
+{
+	long lReturn;
+
+	switch(iRadix)
+	{
+	case 16:
+		lReturn = kHexStringToQword(pcBuffer);
+		break;
+	case 10:
+	default:
+		lReturn = kDecimalStringToLong(pcBuffer);
+		break;
+	}
+	return lReturn;
+}
+
+//Convert hex string to QWORD
+QWORD kHexStringToQword(const char* pcBuffer)
+{
+	QWORD qwValue = 0;
+	int i;
+
+	//Return in turn by rotating the string.
+	for(i=0; pcBuffer[i]!='\0'; i++)
+	{
+		qwValue *= 16;
+		if(('A'<=pcBuffer[i]) && (pcBuffer[i]<='Z'))
+		{
+			qwValue += (pcBuffer[i] - 'A') + 10;
+		}
+		else if(('a'<=pcBuffer[i]) && (pcBuffer[i]<='z'))
+		{
+			qwValue += (pcBuffer[i] - 'a') + 10;
+		}
+		else
+		{
+			qwValue += pcBuffer[i] - '0';
+		}
+	}
+	return qwValue;
+}
+
+//Convert decimal string to long
+long kDecimalStringToLong(const char* pcBuffer)
+{
+	long lValue = 0;
+	int i;
+
+	if(pcBuffer[0] == '-')
+	{
+		i = 1;
+	}
+	else
+	{
+		i = 0;
+	}
+
+	//Return in turn by rotating the string.
+	for( ; pcBuffer[i]!='\0'; i++)
+	{
+		lValue *= 10;
+		lValue += pcBuffer[i] - '0';
+	}
+
+	//If negative number, add minus sign.
+	if(pcBuffer[0] == '-')
+	{
+		lValue = -lValue;
+	}
+	return lValue;
+}
+
+//Internal implementation of itoa()
+int kIToA(long lValue, char* pcBuffer, int iRadix)
+{
+	int iReturn;
+
+	switch(iRadix)
+	{
+		//hex
+	case 16:
+		iReturn = kHexToString(lValue, pcBuffer);
+		break;
+
+	case 10:
+	default:
+		iReturn = kDecimalToString(lValue, pcBuffer);
+		break;
+	}
+
+	return iReturn;
+}
+
+//Convert hex value to string.
+int kHexToString(QWORD qwValue, char* pcBuffer)
+{
+	QWORD i;
+	QWORD qwCurrentValue;
+
+	//Process as soon as 0 is delivered.
+	if(qwValue == 0)
+	{
+		pcBuffer[0] = '0';
+		pcBuffer[1] = '\0';
+		return 1;
+	}
+
+	for(i=0; qwValue > 0; i++)
+	{
+		qwCurrentValue = qwValue % 16;
+		if(qwCurrentValue >= 10)
+		{
+			pcBuffer[i] = 'A' + (qwCurrentValue - 10);
+		}
+		else
+		{
+			pcBuffer[i] = '0' + qwCurrentValue;
+		}
+
+		qwValue = qwValue / 16;
+	}
+	pcBuffer[i] = '\0';
+
+	kReverseString(pcBuffer);
+	return i;
+}
+
+//Convert decimal value to string
+int kDecimalToString(long lValue, char* pcBuffer)
+{
+	long i;
+
+	if(lValue == 0)
+	{
+		pcBuffer[0] = '0';
+		pcBuffer[1] = '\0';
+		return 1;
+	}
+
+	if(lValue < 0)
+	{
+		i = 1;
+		pcBuffer[0] = '-';
+		lValue = -lValue;
+	}
+	else
+	{
+		i = 0;
+	}
+
+	for( ; lValue > 0 ; i++)
+	{
+		pcBuffer[i] = '0' + lValue % 10;
+		lValue = lValue / 10;
+	}
+	pcBuffer[i] = '\0';
+
+	if(pcBuffer[0] == '-')
+	{
+		kReverseString(&(pcBuffer[1]));
+	}
+	else
+	{
+		kReverseString(pcBuffer);
+	}
+
+	return i;
+}
+
+
 
